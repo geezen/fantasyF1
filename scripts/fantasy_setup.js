e1e901b187ea89522a1db3e4e254da01de0676b9
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2026-03-04 13:10:59 +0100

    Basic files

---
+++ scripts/fantasy_setup.js
@@ -0,0 +1,18 @@
+const players = [
+    {
+        name: "Marcus",
+        drivers: [63, 6, 41, 27, 5]
+    },
+    {
+        name: "Axel",
+        drivers: [3, 12, 87, 31, 43]
+    },
+    {
+        name: "Sixten",
+        drivers: [16, 81, 14, 10, 18]
+    },
+    {
+        name: "David",
+        drivers: [1, 44, 55, 30, 11]
+    }
+]
