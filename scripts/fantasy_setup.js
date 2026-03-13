cab7a9e001df40920a36e602c7d7b77d25fa9a62
marcusgerhardsson <gerhardsson.marcus@gmail.com>
2026-03-13 23:16:19 +0100

    working MVP

--- scripts/fantasy_setup.js
+++ scripts/fantasy_setup.js
@@ -1,4 +1,4 @@
-/*const year = "2026";
+const year = "2026";
 
 const players = [
     {
@@ -7,7 +7,7 @@ const players = [
     },
     {
         name: "Axel",
-        drivers: [4, 12, 87, 31, 43]
+        drivers: [3, 12, 87, 31, 43]
     },
     {
         name: "Sixten",
@@ -16,25 +16,5 @@ const players = [
     {
         name: "David",
         drivers: [1, 44, 55, 30, 11]
-    }
-];*/
-const year = "2025";
-
-const players = [
-    {
-        name: "Marcus",
-        drivers: [7,1,14,10,18]
-    },
-    {
-        name: "Axel",
-        drivers: [16,44,31,87,5]
-    },
-    {
-        name: "Sixten",
-        drivers: [27,4,81,12,63]
-    },
-    {
-        name: "David",
-        drivers: [6,22,30,23,55]
     }
 ];
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,32 @@
 const driverMap = new Map();
-const grandPrix = [];
+const sessions = [];
 
 async function main() {
     const standingsTable = document.getElementById("standings-table");
+    await fetchSessions();
     await fetchDriverInfo();
-    await fetchGrandPrix();
-    await new Promise(r => setTimeout(r, 1000));
     await fetchChampionshipResults();
+    sortDrivers();
+    sortPlayers();
     fillTable(players, standingsTable);
 }
 
-async function fetchDriverInfo() {
+async function fetchSessions() {
     let data = await cachefetch(`https://api.openf1.org/v1/sessions?year=${year}`);
-    const sessionKey = data[0].session_key;
-    console.log(`A session_key for ${year} is ${sessionKey}`);
+    data.forEach(session => {
+        sessions.push(session);
+    });
+    sessions.sort((a, b) => new Date(a.date_start) - new Date(b.date_start));
+}
 
+async function fetchDriverInfo() {
     allDriverNbrs = [];
     players.forEach(player => {
         allDriverNbrs.push(...player.drivers);
     });
 
     console.log(`Fetching info for drivers ${allDriverNbrs}`);
-    let url = `https://api.openf1.org/v1/drivers?session_key=${sessionKey}`
+    let url = `https://api.openf1.org/v1/drivers?session_key=${sessions[0].session_key}`
     allDriverNbrs.forEach(nbr => {
         url += `&driver_number=${nbr}`;
     });
@@ -32,19 +37,19 @@ async function fetchDriverInfo() {
     });
 }
 
-async function fetchGrandPrix() {
-    const data = await cachefetch(`https://api.openf1.org/v1/meetings?year=${year}`);
-    data.forEach(meeting => {
-        if (meeting.meeting_name.toLowerCase().includes("grand prix")) {
-            grandPrix.push(meeting);
+async function fetchChampionshipResults() {
+    let latestRaceKey = -1;
+    sessions.forEach(session => {
+        let hasHappened = new Date(session.date_end) < new Date();
+        let isRace = session.session_type == "Race";
+        if (hasHappened && isRace) {
+            latestRaceKey = session.session_key;
         }
     });
-    grandPrix.sort((a, b) => new Date(a.date_start) - new Date(b.date_start));
-}
-
-async function fetchChampionshipResults() {
-    // TODO, fix url
-    const data = await cachefetch(`https://api.openf1.org/v1/championship_drivers?meeting_key=1276`);
+    if (latestRaceKey == -1) {
+        console.log(`Unable to find a race that has happened this season`);
+    }
+    const data = await cachefetch(`https://api.openf1.org/v1/championship_drivers?session_key=${latestRaceKey}`);
     data.forEach(result => {
         if (driverMap.has(result.driver_number)) {
             const driver = driverMap.get(result.driver_number);
@@ -74,35 +79,37 @@ function fillTable(players, table) {
     createFilledChild("th", headerRow, "Pos.");
     createFilledChild("th", headerRow, "Player");
     createFilledChild("th", headerRow, "Drivers");
-    grandPrix.forEach(gp => {
-        const innerHTML = `${gp.country_code}<br><img src=${gp.country_flag} height=12>`;
-        createFilledChild("th", headerRow, innerHTML);
-    });
-    createFilledChild("th", headerRow, "Points");
+    createFilledChild("th", headerRow, "Driver pts.");
+    createFilledChild("th", headerRow, "Player pts.");
 
     // Player rows
-    players.forEach(player => {
+    players.forEach((player, index) => {
         let first = true;
+        const fullRowSpan = player.drivers.length;
         player.drivers.forEach(driverNbr => {
             const playerRow = createChild("tr", table);
             if (first) {
-                const posCol = createFilledChild("td", playerRow, "0");
-                posCol.rowSpan = player.drivers.length;
-                const nameCol = createFilledChild("td", playerRow, player.name);
-                nameCol.rowSpan = player.drivers.length;
-                first = false;
+                const posCol = createFilledChild("td", playerRow, index + 1);
+                posCol.rowSpan = fullRowSpan;
+                const playerNameCol = createFilledChild("td", playerRow, player.name);
+                playerNameCol.rowSpan = fullRowSpan;
             }
-            driverData = driverMap.get(driverNbr);
-            console.log(driverData, driverNbr);
-            const nameTd = createFilledChild("td", playerRow, driverData["name_acronym"]);
-            nameTd.style.backgroundColor = `#${driverData["team_colour"]}`;
-            grandPrix.forEach(gp => {
-                createFilledChild("td", playerRow, "&nbsp;");
-            });
-            const points = driverData.result.points_current;
-            createFilledChild("td", playerRow, points);
+            let driverData = driverMap.get(driverNbr);
+            const driverNameCol = createFilledChild("td", playerRow, driverData["name_acronym"]);
+            driverNameCol.style.backgroundColor = `#${driverData["team_colour"]}`;
+            createFilledChild("td", playerRow, getDriverPoints(driverNbr));
+            if (first) {
+                const playerPtsCol = createFilledChild("td", playerRow, player.points);
+                playerPtsCol.rowSpan = fullRowSpan;
+            }
+            first = false;
         });
     });
+}
+
+function getDriverPoints(driverNbr) {
+    const driverData = driverMap.get(driverNbr);
+    return driverData.result.points_current ?? 0;
 }
 
 function createChild(type, parent) {
@@ -115,6 +122,27 @@ function createFilledChild(type, parent, innerHTML) {
     const element = createChild(type, parent);
     element.innerHTML = innerHTML;
     return element;
+}
+
+function sortDrivers() {
+    players.forEach(player => {
+        player.drivers.sort((a, b) => getDriverPoints(b) - getDriverPoints(a));
+    });
+}
+
+function sortPlayers() {
+    sumPlayerPoints();
+    players.sort((a, b) => b.points - a.points);
+}
+
+function sumPlayerPoints() {
+    players.forEach(player => {
+        let points = 0;
+        player.drivers.forEach(driverNbr => {
+            points += getDriverPoints(driverNbr);
+        });
+        player.points = points;
+    });
 }
 
 main()
